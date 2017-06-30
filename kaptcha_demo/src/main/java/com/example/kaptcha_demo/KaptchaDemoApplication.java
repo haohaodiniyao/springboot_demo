1c13bfc2a67721d53ca2181910647b7b988380f1
yaokai <yaokaiabc@gmail.com>
2017-06-30 11:01:50 +0800

    图片验证码生成kaptcha

---
+++ kaptcha_demo/src/main/java/com/example/kaptcha_demo/KaptchaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.kaptcha_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaptchaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaptchaDemoApplication.class, args);
+	}
+}
